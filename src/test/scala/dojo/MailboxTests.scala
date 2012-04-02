d0fdbcdaa10a7900ce3cc404f982b0635b8b73f0
Adam Ruka <adamruka85@gmail.com>
2012-04-02 22:17:02 +0200

    Added spaces before opening braces.

--- src/test/scala/dojo/MailboxTests.scala
+++ src/test/scala/dojo/MailboxTests.scala
@@ -12,38 +12,38 @@ class MailboxTests extends FunSuite with ShouldMatchers {
     response should equal (Pong)
   }
 
-  test("should return ping when sent pong"){
+  test("should return ping when sent pong") {
     val response = new MailBox().handleMsg(Pong)
     response should equal (Ping)
   }
 
-  test("should return pong when received ping"){
+  test("should return pong when received ping") {
     val response = new MailBox().receive(Ping)
     response should equal (Pong)
   }
 
-  test("should return ping when received pong"){
+  test("should return ping when received pong") {
     val response = new MailBox().receive(Pong)
     response should equal (Ping)
   }
 
-  test("should return stopped when received stop message"){
+  test("should return stopped when received stop message") {
     val response = new MailBox().receive("stop")
     response should equal ("stopped")
   }
 
-  test("should throw exception when go message received"){
+  test("should throw exception when go message received") {
     intercept[MatchError] {
       new MailBox().receive("go")
     }
   }
 
-  test("should return Hello World when received echo message"){
+  test("should return Hello World when received echo message") {
     val response = new MailBox().receive(("echo", "Hello World"))
     response should equal ("Hello World")
   }
 
-  test("should return Goodbye World when received echo message with Goodbye World payload"){
+  test("should return Goodbye World when received echo message with Goodbye World payload") {
     val response = new MailBox().receive(("echo", "Goodbye World"))
     response should equal ("Goodbye World")
   }
